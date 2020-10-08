a963ed84495eb35f8fd5534f623ecd4c2788542e
xqmmcqs <xqmmcqs@163.com>
2020-10-08 21:22:22 +0800

    update recursion.js

--- Lab1/recursion.js
+++ Lab1/recursion.js
@@ -1,8 +1,22 @@
 function cal(n, x, y) {
     var res = 0;
-    for (var i = 2; i <= n; i++)
-    {
+    for (var i = 2; i <= n; i++) {
         res = (res + y) % i;
     }
     return (res + x - 1) % n + 1;
+}
+
+
+function getPoints(n) {
+    var points = [];
+    for (var x = 1; x <= n; x++) {
+        for (var y = 1; y <= n; y++) {
+            points.push({
+                x: x,
+                y: y,
+                z: cal(n, x, y)
+            });
+        }
+    }
+    return points;
 }
