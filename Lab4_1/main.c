6b43b985dae885c09501b750be9c340ce5f6cfb7
xqmmcqs <xqmmcqs@163.com>
2020-11-04 08:26:48 +0800

    update lab4_1 algorithm

---
+++ Lab4_1/main.c
@@ -0,0 +1,58 @@
+//
+// Created by xqmmcqs on 2020/11/2.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct node
+{
+    char data;
+    struct node *lc, *rc;
+} Node;
+
+Node *buildTree()
+{
+    char ch = getchar();
+    if (ch == '*')
+        return NULL;
+    Node *point = (Node *)calloc(1, sizeof(Node));
+    point->data = ch;
+    point->lc = buildTree();
+    point->rc = buildTree();
+    return point;
+}
+
+void preorderTraverse(Node *point)
+{
+    printf("%c ", point->data);
+    if (point->lc)
+        printf("%c ", point->lc->data);
+    else printf("*");
+    if (point->rc)
+        printf("%c ", point->rc->data);
+    else printf("*");
+    puts("");
+    if (point->lc)
+        preorderTraverse(point->lc);
+    if (point->rc)
+        preorderTraverse(point->rc);
+}
+
+void destroyTree(Node *point)
+{
+    if (point->lc)
+        destroyTree(point->lc);
+    if (point->rc)
+        destroyTree(point->rc);
+    free(point);
+}
+
+int main()
+{
+    Node *root = buildTree();
+    preorderTraverse(root);
+    destroyTree(root);
+    return 0;
+}
