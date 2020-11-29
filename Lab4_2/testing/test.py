edb2cabca7061a3dca3589a34ef970a2d06cfd4f
xqmmcqs <xqmmcqs@163.com>
2020-11-29 21:09:02 +0800

    finished lab4_1 4_2 report

--- Lab4_2/testing/test.py
+++ Lab4_2/testing/test.py
@@ -5,7 +5,7 @@ with open("./result.txt", "w") as file:
     for i in (10, 30, 50, 100, 500, 1000, 10000, 1000000):
         for j in (3, 6, 10, 20, 30, 50, 95):
             tot = 0
-            for k in range(10):
+            for k in range(100):
                 time.sleep(1)
                 os.system("./data {0} {1}".format(i, j))
                 os.system("./main 1 raw.in out.out")
